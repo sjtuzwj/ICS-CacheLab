0a7efdb8d40f46c23eb44cd91f203f824dbd9477
Wenjie <32933716+sjtuzwj@users.noreply.github.com>
2019-03-22 13:45:11 +0800

    Enum to readability

--- cache/csim.c
+++ cache/csim.c
@@ -8,6 +8,17 @@ ID: ics517021910799
 */
 int hitn=0, missn=0, evictn=0;
 
+enum Bool{
+    True=1,
+    False=!True
+};
+
+enum HitFlag{
+    Hit=1,
+    Miss=0,
+    Evict=-1
+};
+
 //正确写法是MTF，不过C写起来太累了，这里直接记录最后一次访问的位次n，然后遍历集找n最小的地方,时间复杂度O(e)。
 int visittime=0;
 
@@ -25,7 +36,7 @@ Info ParseShell(int argc, char* argv[])
     Info info;
     if(argc==10){
         //verbose
-        info.verbose=1;
+        info.verbose=True;
         info.file=argv[9];
         info.s=atoi(argv[3]);
         info.E=atoi(argv[5]);
@@ -33,7 +44,7 @@ Info ParseShell(int argc, char* argv[])
     }
     else if(argc==9){
         //normal
-        info.verbose=0;
+        info.verbose=False;
         info.file=argv[8];
         info.s=atoi(argv[2]);
         info.E=atoi(argv[4]);
@@ -66,7 +77,7 @@ typedef struct Line{
     size_t visit;
     size_t b;
     size_t tag;
-    int valid;
+    Bool valid;
     byte* blocks;//vector
 }Line;
 
@@ -86,7 +97,7 @@ void GenerateLine(Line* ret,size_t b)
     ret->visit=0;
     ret->b=b;
     ret->tag=0;
-    ret->valid=0;
+    ret->valid=False;
     ret->blocks=byte*(malloc(b));
 }
 
@@ -126,19 +137,19 @@ int checkHit(size_t address, Line* line)
     visittime++;
     line->visit=visittime;
     int tag=GetBlockIndex(address,info);
-    if(line->valid==1)
+    if(line->valid==True)
     {
         if(line->tag==tag)
-        return 1;//hit
+        return Hit;//hit
         else{
             line->tag=tag;
-            return -1;//evict
+            return Evict;//evict
         }
     }
     else{
         line->tag=tag;//miss
-        line->valid=1;
-        return 0;
+        line->valid=True;
+        return Miss;
     }
 }
 
@@ -150,19 +161,19 @@ int visit(Info* info, Cache* cache, size_t address)
     return checkHit(address,curline);
 }
 
-void ParseFlag(int flag)
+void ParseFlag(HitFlag flag)
 {
     switch(flag)
     {
-        case -1:
+        case Evict:
         evictn++;
         printf("eviction ");
         return;
-        case 0:
+        case Miss:
         missn++;
         printf("miss ");
         return;
-        case 1:
+        case Hit:
         hitn++;
         printf("hit ");
     }
